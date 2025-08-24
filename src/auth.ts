1de55a4a61fd2132a84d4e7045151ce6a28baa9b
shabdpreetz <shabdpreetz@gmail.com>
2025-08-24 11:30:37 +0530

    adding redirct_uri in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,26 +3,36 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import { prisma } from "@/lib";
 
-
-if(!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET ){
-    throw new Error("Missing Github client id or client secret");
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing Github client id or client secret");
 }
 
-
-export const {handlers: {GET,POST}, auth, signIn, signOut} = NextAuth({   // it return object i destructor it
-    adapter: PrismaAdapter(prisma),
-    providers:[
-        GithubProvider({
-            clientId:process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
-        })
-    ],
-    callbacks:{
-        async session({user, session}){
-            if(session && user){
-                session.user.id = user.id
-            }
-            return session
-        }
-    }
-})
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  // it return object i destructor it
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ user, session }) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs or same-origin URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl; // Default to home page or customize as needed
+    },
+  },
+});
